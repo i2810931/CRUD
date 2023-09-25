af8832a7ca2480d94e102ff06bc9ba0707caad05
i2810931 <145955012+i2810931@users.noreply.github.com>
2023-09-24 23:52:28 -0500

    First commit

---
+++ registro.php
@@ -0,0 +1,24 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+include('db.php');
+
+$nombre=$_POST['txtNombre'];
+$usuario=$_POST['txtUsuario'];
+$contraseña=$_POST['txtContraseña'];
+
+$consulta="INSERT INTO `usuarios` (`Nombre`, `Usuario`, `Contraseña`)
+VALUES ('$nombre', '$usuario', '$contraseña')";
+
+$resultado=mysqli_query($conexion,$consulta) or die("error de registro");
+
+echo "registro exitoso";
+
+mysqli_close($conexion);
+
+
+
+
+
+?>
